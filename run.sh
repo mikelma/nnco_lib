17018301a2050932eb0f14af1283d3c5bfae40e6
mikelma <mikelma7@gmail.com>
2022-04-28 17:49:52 +0000

    more test cases added

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 LR=(0.01 0.001 0.0001 0.00001 0.000001)
-PREHEADS=(0 1 2 3 4)
+PREHEADS=(0 1 2 3 4 5 6)
 
 batch_size=32
 noise_dim=128
 hidden_dim=128
 
-# instance="../instances/PFSP/tai20_5_8.fsp"
-instance="../instances/PFSP/tai50_20_8.fsp"
+instance="../instances/PFSP/tai20_5_8.fsp"
+# instance="../instances/PFSP/tai50_20_8.fsp"
 problem="pfsp"
 
 iters=0
